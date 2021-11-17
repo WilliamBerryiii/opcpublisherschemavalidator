62e859f6b2531569c2da34b0787a72ffbe54e702
Bill Berry <WilliamBerryiii@users.noreply.github.com>
2021-11-17 12:55:06 -0800

    Feature/opc pub compat (#5)
    * add gvv feature to disable value checks and add expandednodeid to current schema
    
    * add example historical pn.json file test for backward compat.

--- test/publishednodes-schema-generator-tests.ts
+++ test/publishednodes-schema-generator-tests.ts
@@ -30,7 +30,7 @@ describe('An example publishednodes.json file', () => {
     expect(validationErrors).to.be.null;
   });
 
-  it('should not return an error when being validated', () => {
+  it('should not return an error when validating a mixed-mode schema', () => {
     const mixedModeSchema = [
       {
         EndpointUrl: 'opc.tcp://localhost:50000',
@@ -58,6 +58,83 @@ describe('An example publishednodes.json file', () => {
 
     const [result, validationErrors] = validateFile(mixedModeSchema, schema);
     //console.log(validationErrors);
+
+    expect(result).to.be.true;
+    expect(validationErrors).to.be.null;
+  });
+
+  it('should not return an error on a highly customized and multi-version capable pn.json file', () => {
+    const historicalCompatibilitySchema = getPublishedNodesSchema(
+      // NOTE: The boolean value below disables ID value checking to support
+      // backward compatibility for some example pn.json files. The official
+      // schema hosted in OPC Publisher uses this setting == false as below.
+      false,
+      [
+        NodeIdFormat.ExpandedNodeId,
+        NodeIdFormat.NamespaceIndex,
+        NodeIdFormat.NodeId,
+      ],
+      true,
+      false
+    );
+
+    const legacyConfigExample = [
+      {
+        EndpointUrl: 'opc.tcp://crpogace01:49320',
+        UseSecurity: false,
+        OpcNodes: [
+          {
+            DisplayName: 'Kep_1_DisplayName',
+            DataSetFieldId: 'Kep_1_DataSetFieldId',
+            Id: 'nsu=KEPServerEX;s=Sim.CH1.SIM_CH1_TAG1\\234754a-c63-b9601',
+            OpcSamplingInterval: 1000,
+            OpcPublishingInterval: 1000,
+          },
+          {
+            DisplayName: 'Kep_2_DisplayName',
+            DataSetFieldId: 'Kep_2_DataSetFieldId',
+            Id: 'ns=2;s=Sim.CH1.SIM_CH1_TAG10\\2347798-c63-19401',
+            ExpandedNodeId:
+              'nsu=KEPServerEX;s=Sim.CH1.SIM_CH1_TAG10\\2347798-c63-19401',
+            OpcSamplingInterval: 1000,
+            OpcPublishingInterval: 1000,
+          },
+        ],
+      },
+      {
+        EndpointUrl: 'opc.tcp://crpogace01:51210/UA/DemoServer',
+        UseSecurity: false,
+        OpcNodes: [
+          {
+            DisplayName: 'Softing_1_DisplayName',
+            DataSetFieldId: 'Softing_1_DataSetFieldId',
+            Id: 'http://test.org/UA/Data/#i=10847',
+            OpcSamplingInterval: 1000,
+            OpcPublishingInterval: 1000,
+          },
+          {
+            DisplayName: 'Softing_2_DisplayName',
+            DataSetFieldId: 'Softing_2_DataSetFieldId',
+            Id: 'nsu=http://test.org/UA/Data/;i=10848',
+            OpcSamplingInterval: 1000,
+            OpcPublishingInterval: 1000,
+          },
+          {
+            DisplayName: 'Softing_3_DisplayName',
+            DataSetFieldId: 'Softing_3_DataSetFieldId',
+            Id: 'ns=3;i=10849',
+            ExpandedNodeId: 'http://test.org/UA/Data/#i=10849',
+            OpcSamplingInterval: 1000,
+            OpcPublishingInterval: 1000,
+          },
+        ],
+      },
+    ];
+    const [result, validationErrors] = validateFile(
+      legacyConfigExample,
+      historicalCompatibilitySchema
+    );
+    console.log(validationErrors);
 
     expect(result).to.be.true;
     expect(validationErrors).to.be.null;
